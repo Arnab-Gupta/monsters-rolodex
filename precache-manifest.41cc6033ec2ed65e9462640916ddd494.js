dbaa612e81e328c8d2cd597d9e5d855b3bd8c401
Arnab Gupta <guptaarnab639@gmail.com>
2020-07-01 03:54:23 +0530

    Updates

---
+++ precache-manifest.41cc6033ec2ed65e9462640916ddd494.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9412b6a7bb95991162bc31e5774cdbdc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b43556ba390a7b466af0",
+    "url": "/static/css/main.52e2f05e.chunk.css"
+  },
+  {
+    "revision": "e9680ee62bee18642f9b",
+    "url": "/static/js/2.9b83ecc0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.9b83ecc0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b43556ba390a7b466af0",
+    "url": "/static/js/main.471649d4.chunk.js"
+  },
+  {
+    "revision": "299bd36d5458990039c2",
+    "url": "/static/js/runtime-main.5fa3ce95.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
